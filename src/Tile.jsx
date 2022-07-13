bc9d1f645705c33780760ff5cebfd58a17d4ec3c
Roy Hershkovitz <roy.hershkovitz@autodesk.com>
2022-07-13 08:51:40 +0300

    before solution

--- src/Tile.jsx
+++ src/Tile.jsx
@@ -1,31 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 
 export const Tile = ({ countDownSeconds }) => {
-  const [timeToDisplay, setTimeToDisplay] = useState(countDownSeconds);
-  const [progress, setProgress] = useState(0);
-
-  const timer = setTimeout(() => {
-    setTimeToDisplay(timeToDisplay - 1);
-  }, 1000);
-  if (timeToDisplay === 0) clearTimeout(timer);
-
-  useEffect(() => {
-    requestAnimationFrame(() => {
-      setProgress(-100);
-    });
-  }, []);
 
   return (
     <div className="tile">
-      <div className="timer">{timeToDisplay}</div>
+      <div className='timer'>0</div>
       <div className="progress-bar__container">
-        <div
-          className="progress-bar"
-          style={{
-            transform: `translateX(${progress}%)`,
-            transition: `transform ${countDownSeconds}s linear`
-          }}
-        ></div>
+        <div className='progress-bar' />
       </div>
       <div>Lorem Ipsum</div>
     </div>
